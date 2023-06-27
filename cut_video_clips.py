1a5e92f73ade696ddb7e28c7b3ba201ec5ad8861
Darek Markiewicz <darekm101@gmail.com>
2023-06-26 22:52:42 -0500

    Fixed a bug videos shorter than minimum, were allowed in

--- cut_video_clips.py
+++ cut_video_clips.py
@@ -29,7 +29,12 @@ def cut_video_clips(configs):
                 total_video_length = float(subprocess.check_output(video_length_command).decode('utf-8').strip())
                 
                 # Get random start time, ensuring it is less than the end time
-                start_time = random.uniform(0, max(0, total_video_length - duration))
+                # First get the max possible start time
+
+                max_start_time = total_video_length - configs['minimum_clip_duration']
+                start_time = random.uniform(0, max(0, total_video_length - max_start_time))
+
+                print(f"Cutting video {source_video} from {start_time} to {start_time + duration}...source video length: {total_video_length}")
 
                 # Generate output video file path
                 output_file = os.path.join(configs['temp_videos'], f"{os.path.basename(source_video)}-clip-{i}.mp4")
@@ -47,10 +52,20 @@ def cut_video_clips(configs):
                 #     'ffmpeg', '-y', '-i', source_video, '-ss', str(start_time), '-t', str(duration),
                 #     '-c:v', 'libx264', '-c:a', 'aac', output_file
                 # ]
-
+                # ffmpeg command to cut video without transcodeing, but can cause jitter in video
                 command = ['ffmpeg', '-y', '-ss', str(start_time), '-i', source_video, '-ss', '0', '-t', str(duration), '-c', 'copy', output_file]
 
                 
+                # #Increase contrast and saturation by 20%
+                # command = ['ffmpeg', '-y', '-i', source_video, '-ss', str(start_time), '-t', str(duration),
+                #            '-vf', 'eq=contrast=1.1:saturation=1.2', '-c:v', 'libx264', '-c:a', 'aac', output_file]
+
+                #I'm goign to try to use curves. 
+    #             command = ['ffmpeg', '-y', '-i', source_video, '-ss', str(start_time), '-t', str(duration),
+    # '-vf', 'curves=all=\'0/0 0.5/0.46 1/1\'', '-c:v', 'libx264', '-c:a', 'aac', output_file]
+
+
+
                 # Run ffmpeg command
                 subprocess.run(command, check=True)
         
@@ -58,47 +73,3 @@ def cut_video_clips(configs):
         with open(config_file_path, 'w') as json_file:
             json.dump(video_configs, json_file, indent=4)
             print(f"Updated video config file: {config_file_path}")
-
-
-
-# import os
-# import json
-# import subprocess
-
-# def cut_video_clips(configs):
-#     print(f"cut_video_clips()")
-#     print(f"Processing video configs in directory: {configs['temp_video_configs']}")
-
-#     # Create temp videos directory if not exist
-#     os.makedirs(configs['temp_videos'], exist_ok=True)
-
-#     # Read video config files
-#     video_config_files = [f for f in os.listdir(configs['temp_video_configs']) if f.endswith(".json")]
-    
-#     for config_file in video_config_files:
-#         config_file_path = os.path.join(configs['temp_video_configs'], config_file)
-#         with open(config_file_path, 'r') as json_file:
-#             video_configs = json.load(json_file)
-#             clips = video_configs['clips']
-
-#             for i, clip in enumerate(clips):
-#                 source_video = clip['source_video']
-#                 duration = clip['duration_from_last_video_cut_point']
-
-#                 # Generate output video file path
-#                 output_file = os.path.join(configs['temp_videos'], f"{os.path.basename(source_video)}-clip-{i}.mp4")
-#                 clip['source_trimmed_video'] = output_file
-
-#                 # ffmpeg command to cut video without transcoding
-#                 command = [
-#                     'ffmpeg', '-y', '-i', source_video, '-ss', '0', '-t', str(duration),
-#                     '-c', 'copy', output_file
-#                 ]
-                
-#                 # Run ffmpeg command
-#                 subprocess.run(command, check=True)
-        
-#         # Overwrite the video config file with updated data
-#         with open(config_file_path, 'w') as json_file:
-#             json.dump(video_configs, json_file, indent=4)
-#             print(f"Updated video config file: {config_file_path}")
--- get_audio_meta.py
+++ get_audio_meta.py
@@ -3,11 +3,13 @@ from scipy.io import wavfile
 import numpy as np
 import json
 import os
+import librosa
 
 def get_audio_meta(config): 
 
     directory = config["source_audio_files"]
     files = os.listdir(directory)
+    min_clip_duration = config["minimum_clip_duration"]
 
     file = next((f for f in files if f.endswith(('.mp3', '.wav'))), None)
 
@@ -34,38 +36,40 @@ def get_audio_meta(config):
         file_path = "file.wav"
 
     print(f"Reading audio file: {file_path}...")
-    sample_rate, data = wavfile.read(file_path)
+    y, sr = librosa.load(file_path)
 
-    duration = len(data) / sample_rate
-    print(f"Audio duration: {duration} seconds")
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
-    if len(data.shape) > 1:
-        data = np.mean(data, axis=1)
+    json_data = []
+    sequence_number = 1
+    video_clip_duration = 0
+    video_sequence_id = 1
 
-    bin_size = 0.1
-    points_per_bin = int(sample_rate * bin_size)
+    for i in range(len(beat_frames)):
+        if i == 0:
+            duration_from_last = librosa.frames_to_time(beat_frames[i], sr=sr)
+        else:
+            duration_from_last = librosa.frames_to_time(beat_frames[i] - beat_frames[i - 1], sr=sr)
 
-    print(f"Calculating moving average with bin size: {bin_size} seconds and points per bin: {points_per_bin}...")
-    moving_avg = np.convolve(data, np.ones(points_per_bin) / points_per_bin, mode='same')
-    moving_stddev = np.array([np.std(data[max(0,i-points_per_bin//2):i+points_per_bin//2]) for i in range(len(data))])
+        video_clip_duration += duration_from_last
 
-    print("Finding spikes...")
-    threshold = 3
-    cooldown = int(sample_rate)  # cooldown of 1 second
-    last_spike = -cooldown
-    spikes = []
-    for i in range(len(data)):
-        if data[i] > moving_avg[i] + threshold * moving_stddev[i] and i > last_spike + cooldown:
-            spikes.append(i)
-            last_spike = i
+        item = {
+            "sequence_number": sequence_number,
+            "duration_from_last": duration_from_last
+        }
 
-    spikes_seconds = list(np.array(spikes) / sample_rate)
+        if video_clip_duration >= min_clip_duration:
+            item["video_cut_point"] = True
+            item["duration_from_last_video_cut_point"] = video_clip_duration
+            video_clip_duration = 0
+            video_sequence_id += 1
+        else:
+            item["video_cut_point"] = False
 
-    json_data = [{"sequence_number": i,
-            "duration_from_last": spikes_seconds[i+1] - spikes_seconds[i] if i+1 < len(spikes_seconds) else spikes_seconds[i+1],
-            "duration_from_start": spikes_seconds[i+1],
-            "video_cut_point": True,
-            "video_sequence_id": i} for i in range(1, len(spikes_seconds)-1)]
+        item["video_sequence_id"] = video_sequence_id
+        json_data.append(item)
+
+        sequence_number += 1
 
     print("Writing output to JSON file...")
     with open(json_path, "w") as json_file:
@@ -79,3 +83,88 @@ if __name__ == "__main__":
     config = read_configs.read_environment()
     
     get_audio_meta(config)
+
+
+
+
+# from pydub import AudioSegment
+# from scipy.io import wavfile
+# import numpy as np
+# import json
+# import os
+
+# def get_audio_meta(config): 
+
+#     directory = config["source_audio_files"]
+#     files = os.listdir(directory)
+
+#     file = next((f for f in files if f.endswith(('.mp3', '.wav'))), None)
+
+#     if file is None:
+#         print(f"No mp3 or wav files found in the directory {directory}.")
+#         exit()
+
+#     file_path = os.path.join(directory, file)
+
+#     output_directory = config["temp_audio_configs"]
+#     os.makedirs(output_directory, exist_ok=True)
+
+#     json_path = os.path.join(output_directory, f'{file.split(".")[0]}.json')
+
+#     # check if the output JSON file already exists
+#     if os.path.isfile(json_path):
+#         print(f"Output JSON file {json_path} already exists. Skipping processing for file {file}...")
+#         return
+
+#     if file.endswith('.mp3'):
+#         print("Converting mp3 to wav...")
+#         audio = AudioSegment.from_mp3(file_path)
+#         audio.export("file.wav", format="wav")
+#         file_path = "file.wav"
+
+#     print(f"Reading audio file: {file_path}...")
+#     sample_rate, data = wavfile.read(file_path)
+
+#     duration = len(data) / sample_rate
+#     print(f"Audio duration: {duration} seconds")
+
+#     if len(data.shape) > 1:
+#         data = np.mean(data, axis=1)
+
+#     bin_size = 0.1
+#     points_per_bin = int(sample_rate * bin_size)
+
+#     print(f"Calculating moving average with bin size: {bin_size} seconds and points per bin: {points_per_bin}...")
+#     moving_avg = np.convolve(data, np.ones(points_per_bin) / points_per_bin, mode='same')
+#     moving_stddev = np.array([np.std(data[max(0,i-points_per_bin//2):i+points_per_bin//2]) for i in range(len(data))])
+
+#     print("Finding spikes...")
+#     threshold = 3
+#     cooldown = int(sample_rate)  # cooldown of 1 second
+#     last_spike = -cooldown
+#     spikes = []
+#     for i in range(len(data)):
+#         if data[i] > moving_avg[i] + threshold * moving_stddev[i] and i > last_spike + cooldown:
+#             spikes.append(i)
+#             last_spike = i
+
+#     spikes_seconds = list(np.array(spikes) / sample_rate)
+
+#     json_data = [{"sequence_number": i,
+#             "duration_from_last": spikes_seconds[i+1] - spikes_seconds[i] if i+1 < len(spikes_seconds) else spikes_seconds[i+1],
+#             "duration_from_start": spikes_seconds[i+1],
+#             "video_cut_point": True,
+#             "video_sequence_id": i} for i in range(1, len(spikes_seconds)-1)]
+
+#     print("Writing output to JSON file...")
+#     with open(json_path, "w") as json_file:
+#             json.dump(json_data, json_file, indent=4)
+
+#     print("Finished.")
+
+# if __name__ == "__main__":
+
+#     import read_configs
+#     config = read_configs.read_environment()
+    
+#     get_audio_meta(config)
